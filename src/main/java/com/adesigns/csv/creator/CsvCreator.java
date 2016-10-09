5da92076ac8cd310dd8cb80a0dcd180db0f1a318
Mike Yudin <mikeyudin@gmail.com>
2016-10-09 11:09:19 -0400

    Add javadoc. Add promotion

--- src/main/java/com/adesigns/csv/creator/CsvCreator.java
+++ src/main/java/com/adesigns/csv/creator/CsvCreator.java
@@ -23,24 +23,43 @@ import java.util.List;
 public class CsvCreator<T> {
 
     private static final char CSV_DELIMITER = ',';
+
     private static final char CSV_QUOTE = '"';
+
     private static final String CSV_ESC_QUOTE = "\"";
 
     private Writer writer;
 
     private Class<T> clazz;
 
+    /**
+     * @param writer Writer to write the CSV output to.
+     * @param clazz The Class that contains the CSV metadata.
+     */
     public CsvCreator(final Writer writer, Class<T> clazz) {
         this.writer = writer;
         this.clazz = clazz;
     }
 
+    /**
+     * Write a list of rows to the output.
+     *
+     * @param csvRows A List containing the rows to write.
+     *
+     * @throws IllegalAccessException If an object cannot be read via reflection.
+     * @throws IOException If there is an issue writing to the writer.
+     */
     public void write(List<T> csvRows) throws IOException, IllegalAccessException {
         for (T row : csvRows) {
             writeRow(row);
         }
     }
 
+    /**
+     * Create a header row in the writer.
+     *
+     * @throws IOException  If there is an issue writing to the writer.
+     */
     public void writeHeaders() throws IOException {
         List<String> headerColumns = ObjectReader.getHeaderColumns(clazz);
         StringBuilder row = new StringBuilder();
@@ -62,6 +81,14 @@ public class CsvCreator<T> {
         writer.write(row.toString());
     }
 
+    /**
+     * Write row to the writer based on the object passed.
+     *
+     * @param csvObject The object that should be written to the writer.
+     *
+     * @throws IllegalAccessException If an object cannot be read via reflection.
+     * @throws IOException If there is an issue writing to the writer.
+     */
     public void writeRow(T csvObject) throws IllegalAccessException, IOException {
         List<Object> rowColumns = ObjectReader.getRow(csvObject);
         StringBuilder row = new StringBuilder();
@@ -83,6 +110,12 @@ public class CsvCreator<T> {
         writer.write(row.toString());
     }
 
+    /**
+     * Prepare a value for CSV output.
+     *
+     * @param value Unescaped value
+     * @return Escaped value
+     */
     private String escapeCsvValue(String value) {
 
         String result = value;
@@ -94,6 +127,12 @@ public class CsvCreator<T> {
         return result;
     }
 
+    /**
+     * Does a value require escaping?
+     *
+     * @param value
+     * @return
+     */
     private boolean requiresCsvEscape(String value) {
         return value != null && (value.contains("\n") || value.contains("\r") || value.indexOf(CSV_DELIMITER) != -1 || value.indexOf(CSV_QUOTE) != -1);
     }
